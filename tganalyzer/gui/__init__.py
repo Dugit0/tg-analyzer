65c74fdc6854a72af931e8d16c255c219e54f7c0
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-04-26 19:09:32 +0300

    Add chat choice combobox

--- tganalyzer/gui/__init__.py
+++ tganalyzer/gui/__init__.py
@@ -6,6 +6,11 @@ import os
 import sys
 
 
+def get_all_chats():
+    """TMP FUNC! Will be removed."""
+    return ["chat00", "chat01", "chat02", "chat03", "chat04", "chat05"]
+
+
 class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
@@ -24,7 +29,10 @@ class MainWindow(QMainWindow):
         data_file_layout.addWidget(self.data_path_label)
         data_file_layout.addWidget(select_data_button)
 
+        self.chat_choice_box = QComboBox(self)
+
         layout.addLayout(data_file_layout)
+        layout.addWidget(self.chat_choice_box)
 
         container = QWidget()
         container.setLayout(layout)
@@ -37,6 +45,7 @@ class MainWindow(QMainWindow):
         if path:
             self.data_path = path
             self.data_path_label.setText(path)
+            self.chat_choice_box.addItems(get_all_chats())
         return
 
 
