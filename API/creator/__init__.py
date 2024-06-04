0703122b5cce0ba266790af6574d656c5d1d7e57
Franky <nikto8748@gmail.com>
2024-06-04 15:57:17 +0300

    Wrote as rst said

--- API/creator/__init__.py
+++ API/creator/__init__.py
@@ -7,14 +7,14 @@ import json
 #Constants and arrays
 
 DATA_PATH = "/home/artiomka/Desktop/tg-data/result.json"
-
+required_fields_message = ['date', 'date_unixtime', 'from', 'from_id', 'id', 'text', 'text_entities', 'type']
+optional_fields_message = ['author', 'contact_information', 'duration_seconds', 'edited', 'edited_unixtime', 'file', 'file_name', 'forwarded_from', 'game_description', 'game_link', 'game_title', 'height', 'inline_bot_buttons', 'location_information', 'media_type', 'mime_type', 'performer', 'photo', 'poll', 'reply_to_message_id', 'reply_to_peer_id', 'saved_from', 'self_destruct_period_seconds', 'sticker_emoji', 'thumbnail', 'title', 'via_bot', 'width']
+required_fields_service = ['action', 'actor', 'actor_id', 'date', 'date_unixtime', 'id', 'text', 'text_entities', 'type']
+optional_fields_service = ['boosts', 'discard_reason', 'duration', 'duration_seconds', 'game_message_id', 'height', 'inviter', 'members', 'message_id', 'new_icon_emoji_id', 'new_title', 'photo', 'score', 'title', 'width']
 
 #extra functions
 
 def game_finder(login, message):
-    '''
-    Finds a place of login in the game.
-    '''
     for line in message["text"]:
         if type(line) is str and line.find(login) != -1:
             return line[1: line.find(".")]
@@ -42,153 +42,90 @@ class Extraction():
         self.chats = self.data['chats']['list']
         return self.chats
 
-
-    def messages_ex_all(self):
-        """
-        Return iterator of all messages.
-        """
-        for chat in self.chats:
-            for message in chat['messages']:
-                yield message
-
-
-    def message_filter(self, message):
-        """
-        Filter for excluding system messages.
-        """
-        return message['type'] != 'service'
-
-
-    def message_keys_ex(self):
-        """
-        Function for finding keys in messages.
-        """
-        self.message_keys = sorted(list({str(list(message.keys())) for message in self.messages_ex_all() if self.message_filter(message)}))
-        return self.message_keys
-
-
-    def req_opt_params_ex(self):
-        '''
-        Finds the requirement params (that are used in every single message) and optional params (that are in specific types of messages)
-	'''
-        self.required_params = []
-        self.optional_params = []
-        message_keys = sorted(list({tuple(message.keys()) for message in self.messages_ex_all() if self.message_filter(message)}))
-        params = sorted(list({par for keys in message_keys for par in keys }))
-        for par in params:
-            if all(map(lambda keys: par in keys, self.message_keys)):
-                self.required_params.append(par)
-            else:
-                self.optional_params.append(par)
-        return (self.required_params, self.optional_params)
+class Chat():
+    """
+    Chat object consists of telegram's chat field's + an array of Message objects.
+    """
+    name = None
+    id = None
+    type = None
+    messages = None
+    def __init__(self, chat):
+        self.name = chat["name"] if "name" in chat.keys() else None
+        self.id = chat["id"]
+        self.type = chat["type"]
+        self.messages = []
 
 class Message():
     """
     Message object consists of telegram's fields + some useful extra fields, like a type, game table etc.
     """
-    #main params
-    msg_login = None   #about who info is
-    msg_type = None   #type of a message
-    '''
-    Available types:
-    "simple_text", "sticker", "voice_message", "video_message", "audio_file", "video_file", "animation", "file", 
-    "photo", "poll", "sticker_emoji", "send_contact", "send_location", 'bot_usage", "game", "unknown"
-    '''
-    msg_game_tables = {}   #game results
+    author = None
+    send_time = None
+    type = None
+    text = None
+    edited = None
+    forwarded = None
 
-    #chat_params
-    chat_name = None   #name of a chat of this message
-    chat_id = None   #id of a chat of this message
-    chat_type = None   #type of a chat of this message
-
-    #required_params
-    mes_date = None   #when it was sent
-    mes_date_unixtime = None   #when it was sent in unixtime
-    mes_from = None   #who sent it
-    mes_from_id = None   #who sent it in id
-    mes_id = None   #message's id
-    mes_text = None   #text in this message
-    mes_text_entities = None
-    '''
-    'bot_command', 'email', 'spoiler', 'mention_name', 'blockquote', 'bold', 'code', 'underline', 'strikethrough', 
-    'phone', 'cashtag', 'text_link', 'italic', 'hashtag', 'plain', 'pre', 'mention', 'custom_emoji', 'link'
-    '''
-    mes_type = None   #telegram type of a message
-
-    #optional_params
-    mes_author = None   #anonim group message
-    mes_contact_information = None   #send to someone else smbd's contact
-    mes_duration_seconds = None   #for gs/circles/videos/audio
-    mes_edited = None   #if edited
-    mes_edited_unixtime = None   #when edited
-    mes_file = None   #data for gs/circles etc
-    mes_forwarded_from = None   #if forwarded
-    mes_game_description = None   #\
-    mes_game_link = None          #| - game messages
-    mes_game_title = None         #/
-    mes_height = None   #stickers + photos
-    mes_inline_bot_buttons = None   #bot buttons under the message
-    mes_location_information = None   #locations
-    mes_media_type = None   #gs, circles, photos, audio, video
-    mes_mime_type = None   #exact type of video, audio etc file
-    mes_performer = None   #singer for audio files
-    mes_photo = None   #if it has photo
-    mes_poll = None   #questionnaire
-    mes_reply_to_message_id = None   #if is it a reply
-    mes_reply_to_peer_id = None   #if it is a reply id version
-    mes_saved_from = None   #reply from another chat
-    mes_self_destruct_period_seconds = None   #destruct message
-    mes_sticker_emoji = None   #not a sticker, but an emoji
-    mes_thumbnail = None   #file, example: pdf
-    mes_title = None   #song title
-    mes_via_bot = None   #bot using
-    mes_width = None   #stickers + photos
-
-    def __init__(self, message, chat, login, req_params):
-        for key in chat.keys():   #its dinamic because we find these params dynamic
-            self.__dict__[f"chat_{key}"] = chat[key]
-        tmp_simple_text = True   #flag for simple text message
-        for key in message.keys():
-            if key not in req_params:
-                if key not in ['edited', 'edited_unixtime', 'forwarded_from', \
-                               'reply_to_message_id', 'reply_to_peer_id']:
-                    tmp_simple_text = False
-            self.__dict__[f"mes_{key}"] = message[key]
-        self.msg_login =  self.mes_from
-        if tmp_simple_text:
-            self.msg_type = "simple_text"
-        elif self.mes_forwarded_from is None and \
-                self.mes_media_type in ["sticker",  #sticker message
-                                        "voice_message",  #gs message
-                                        "video_message",  #circle message
-                                        "audio_file",  #message with an audio file
-                                        "video_file",  #message with a video
-                                        "animation"]:  #message with a gif
-            self.msg_type = self.mes_media_type
-        elif self.mes_mime_type:   #message with a file
-            self.msg_type = "file"
-        elif self.mes_photo:   #message with a photo
-            self.msg_type = "photo"
-        elif self.mes_poll:   #questionnaire
-            self.msg_type = "poll"
-        elif self.mes_sticker_emoji:   #sticker emoji
-            self.msg_type = "sticker_emoji"
-        elif self.mes_contact_information:   #maybe for future
-            self.msg_type = "send_contact"
-        elif self.mes_location_information:   #maybe for future
-            self.msg_type = "send_location"
-        elif self.mes_via_bot:   #using a bot
-            self.msg_type = "bot_usage"
-        elif self.mes_game_title and (place := game_finder(login, message)):   #competition tables
-            self.msg_type = "game"
-            self.msg_game_tables[self.mes_game_title] = place
+    def __init__(self, message, chat, login):
+        self.send_time = message["date"]
+        self.text = message["text"]
+        if message["type"] == "service":
+            self.author = message["actor"]
+            if message["action"] == "phone_call":
+                self.duration = message["duration_seconds"] if "duration_seconds" in message.keys() else 0
+                self.type = "single_call"
+            else:
+                self.duration = message["duration"] if "duration" in message.keys() else 0
+                self.type = "group_call"
         else:
-            self.msg_type = "unknown"
-        '''
-        elif self.mes_self_destruct_period_seconds:   #maybe for future
-            self.msg_type = "self_destruct"
-        '''   #not sure if its needed
-	
+            self.author = message["from"]
+            if "edited" in message.keys():
+                self.edited = True
+                self.edit_time = message["edited"]
+            else:
+                self.edited = False
+            if "forwarded_from" in message.keys():
+                self.forwarded = True
+                self.forwarded_from = message["forwarded_from"]
+            else:
+                self.forwarded = False
+
+            tmp_simple_text = True   #flag for simple text message
+            for key in message.keys():
+                if key not in required_fields_message:
+                    if key not in ['edited', 'edited_unixtime', 'forwarded_from', \
+		                       'reply_to_message_id', 'reply_to_peer_id']:
+                        tmp_simple_text = False
+
+            if tmp_simple_text:
+                self.type = "simple_text"
+            elif "media_type" in message.keys() and message["media_type"] in ["sticker",  #sticker message
+		                                "voice_message",  #gs message
+		                                "video_message",  #circle message
+		                                "audio_file",  #message with an audio file
+		                                "video_file",  #message with a video
+		                                "animation"]:  #message with a gif
+                self.type = message["media_type"]
+                if "duration_seconds" in message.keys():
+                    self.duration = message["duration_seconds"]
+            elif "mime_type" in message.keys():   #message with a file
+                self.type = "file"
+            elif "photo" in message.keys():   #message with a photo
+                self.type = "photo"
+            elif "poll" in message.keys():   #questionnaire
+                self.type = "poll"
+            elif "contact_information" in message.keys():   #maybe for future
+                self.type = "contact"
+            elif "location_information" in message.keys():   #maybe for future
+                self.type = "location"
+            elif "via_bot" in message.keys():   #using a bot
+                self.type = "bot_usage"
+            elif "game_title" in message.keys() and (place := game_finder(login, message)):   #competition tables
+                self.type = "game"
+                self.game_title = message["game_title"]
+                self.game_place = place
+            self.type = "unknown"
 
 
 def start_api(login):
@@ -201,14 +138,17 @@ def start_api(login):
     '''
     extractor = Extraction()
     data = extractor.data_ex()
-    chats = extractor.chats_ex()
-    message_keys = extractor.message_keys_ex()
-    required_params, optional_params = extractor.req_opt_params_ex()
-    messages = []
-    #print(required_params, "\n-------------\n", optional_params)
-    for chat in chats:
+    old_chats = extractor.chats_ex()
+
+    ret_chats = []
+    for chat in old_chats:
+        tmp_chat = Chat(chat)
+        messages = []
         for message in chat['messages']:
-            if message["type"] != "service":
-                tmp_mes = Message(message, chat, login, required_params)
-                messages.append(tmp_mes)
-    return messages
+            if message["type"] == "service" and message["action"] != "phone_call" and message["action"] != "group_call":
+                continue
+            tmp_mes = Message(message, chat, login)
+            messages.append(tmp_mes)
+        tmp_chat.messages = messages
+        ret_chats.append(tmp_chat)
+    return ret_chats
