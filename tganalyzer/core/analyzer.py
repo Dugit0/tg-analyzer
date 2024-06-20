9fdba09261267a79ef189fa122964f3239be862c
Franky <nikto8748@gmail.com>
2024-06-21 01:47:21 +0300

    added documentation

--- tganalyzer/core/analyzer.py
+++ tganalyzer/core/analyzer.py
@@ -7,39 +7,75 @@ from collections import defaultdict
 # Функции подсчета
 
 def counter_symbols(update, message, feat_meaning):
+    """Подсчитывает число символов каждого пользователя в каждый день.
+    
+    :param update: структура для подсчета символов.
+    :type update: dict
+    :param message: анализируемое сообщение.
+    :type chat_data: Message
+    :param feat_meaning: значение, передаваемое при вызове анализатора.
+    :type id: int/bool/str
+    """
     update[message.author][message.send_time.date()] += len(message.text)
 
 
 def counter_words(update, message, feat_meaning):
+    """Подсчитывает число слов каждого пользователя в каждый день.
+    
+    :param update: структура для подсчета слов.
+    :type update: dict
+    :param message: анализируемое сообщение.
+    :type chat_data: Message
+    :param feat_meaning: значение, передаваемое при вызове анализатора.
+    :type id: int/bool/str
+    """
     update[message.author][message.send_time.date()] += len(message.text.split())
 
 
 def counter_msgs(update, message, feat_meaning):
+    """Подсчитывает число сообщений каждого пользователя в каждый день.
+    
+    :param update: структура для подсчета сообщений.
+    :type update: dict
+    :param message: анализируемое сообщение.
+    :type chat_data: Message
+    :param feat_meaning: значение, передаваемое при вызове анализатора.
+    :type id: int/bool/str
+    """
     update[message.author][message.send_time.date()] += 1
 
 
 # Функции подготовки вывода
 
 def return_text_info(update, chat_data, id):
+    """Собирает текстовую информацию в одном месте (символы, слова и тп).
+    
+    :param update: общая дополняемая структура.
+    :type update: dict
+    :param chat_data: информация о символах ,словах и тп об одном чате.
+    :type chat_data: dict
+    :param id: id чата.
+    :type id: int
+    """
     update[id] = chat_data
 
 
 # Константы
 
 DEPENDENCIES = {"symb": {"class_type": defaultdict,
-                         "class_extra_type": defaultdict,
+                         "class_ex_type": defaultdict,
                          "class_func": counter_symbols,
                          "return_type": dict,
                          "return_func": return_text_info
                         },
                 "word": {"class_type": defaultdict,
-                         "class_extra_type": defaultdict,
+                         "class_ex_type": defaultdict,
                          "class_func": counter_words,
                          "return_type": dict,
                          "return_func": return_text_info
                         },
                 "msg": {"class_type": defaultdict,
-                        "class_extra_type": defaultdict,
+                        "class_ex_type": defaultdict,
                         "class_func": counter_msgs,
                         "return_type": dict,
                         "return_func": return_text_info
@@ -65,11 +101,11 @@ class Chat_stat():
 
         for feature in DEPENDENCIES.keys():
             if features[feature]:
-                tmp_type = int
+                tp = int
                 if "class_extra_type" in DEPENDENCIES[feature].keys():
-                    tmp_type = lambda: DEPENDENCIES[feature]["class_extra_type"](int)
+                    tp = lambda: DEPENDENCIES[feature]["class_ex_type"](int)
                 setattr(self, feature, 
-                        DEPENDENCIES[feature]["class_type"](tmp_type))
+                        DEPENDENCIES[feature]["class_type"](tp))
 
         start_mes = bisect.bisect_left(chat.messages, time_gap[0],
                                         key=lambda x : x.send_time)
@@ -79,7 +115,10 @@ class Chat_stat():
             message = chat.messages[i]
             for feature in features.keys():
                 if features[feature]:
-                    DEPENDENCIES[feature]["class_func"](getattr(self, feature), message, features[feature])
+                    DEPENDENCIES[feature]["class_func"](getattr(self, 
+                                                                feature),
+                                                        message,
+                                                        features[feature])
 
 
 # Основные функции
@@ -90,33 +129,35 @@ def start_analyses(parsed_chats, time_gap,
 
     :param parsed_chats: массив объектов класса Chat из creator.
     :type parsed_chats: list
-    :param chat_ids: id чатов, которые будут анализироваться.
-    :type chat_ids: list
     :param time_gap: границы временного интервала.
     :type time_gap: list
     :param features: словарь с необходимыми для подсчета статистик данными.
     :type features: dict
     :param output_folder: папка, в которую пойдут выходные файлы.
     :type output_folder: str
-    :return: словарь, содержащий общую статистику.
-    :rtype: dict
+    :return: массив, содержащий общую статистику и метаданные на отправку
+    для репрезентации.
+    :rtype: list
     """
     features_type = {}
     for feature in features.keys():
         if features[feature]:
             features_type[feature] = DEPENDENCIES[feature]["return_type"]()
 
+    ret_parsed_chats = {}
     for chat in parsed_chats:
+        ret_parsed_chats[chat.id] = chat   # упорядочивание для удобства html
         analysed_chat = Chat_stat(features, chat, time_gap)
-
         for feature in features.keys():
             if features[feature]:
                 DEPENDENCIES[feature]["return_func"](features_type[feature],
-                                                 getattr(analysed_chat, feature), chat.id)
+                                                 getattr(analysed_chat,
+                                                         feature),
+                                                 chat.id)
 
     ret_stats = {}
     for feature in features.keys():
         if features[feature]:
             ret_stats[feature] = features_type[feature]
 
-    return [ret_stats, parsed_chats, time_gap]
+    return [ret_stats, ret_parsed_chats, time_gap]
