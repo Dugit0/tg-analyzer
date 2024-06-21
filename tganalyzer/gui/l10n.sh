7ea2a2af573a682623704440cf94628bb0b7e8bf
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-06-21 13:19:49 +0300

    Fix l10n script

--- tganalyzer/gui/l10n.sh
+++ tganalyzer/gui/l10n.sh
@@ -4,3 +4,4 @@ pybabel extract -o gui.pot __init__.py &&
 pybabel update --previous --ignore-pot-creation-date -D gui -d po -i gui.pot &&
 pybabel compile -D gui -l ru_RU.UTF-8 -d po -i po/ru_RU.UTF-8/LC_MESSAGES/gui.po
 rm gui.pot
+rm po/ru_RU.UTF-8/LC_MESSAGES/gui.mo
